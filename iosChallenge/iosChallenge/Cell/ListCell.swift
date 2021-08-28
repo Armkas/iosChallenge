397de9d1d898ec75ef1ddbc2a8d5e64a522d58a9
aPuYue <pu.yue@access-company.com>
2021-08-28 17:31:46 +0900

    Add Cell

---
+++ iosChallenge/iosChallenge/Cell/ListCell.swift
@@ -0,0 +1,31 @@
+//
+//  listCell.swift
+//  iosChallenge
+//
+//  Created by puyue on R 3/08/28.
+//
+
+import Foundation
+import UIKit
+
+internal final class ListCell: UITableViewCell {
+
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var rateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func bind(rate: Rate) {
+        countryLabel.text = rate.target
+        rateLabel.text = "\(rate.value)"
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
+}
--- iosChallenge/iosChallenge/ViewController.swift
+++ iosChallenge/iosChallenge/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var inputTestField: UITextField!
+    @IBOutlet weak var fromButton: UIButton!
+    @IBOutlet weak var toButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
